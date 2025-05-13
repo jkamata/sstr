707d623682d6591152da619e44b9411cabebec76
git <kamata@tanakatochikanri.com>
2025-05-13 15:38:42 +0900

    first class file added .

--- code/sample.py
+++ code/sample.py
@@ -1,9 +1,16 @@
 import math
 import sys
+import yfinance as yf
+import stock_module
+
 
 def main():
-  val = float(sys.argv[1])
-  print(math.radians(val))
+  STOCK_download = yf.download(tickers="AAPL", period="max")
+  print(STOCK_download)
+  
+  sm = stock_module.StockTool()
+  sm.getPrice(111)
+  return
 
 if __name__ == "__main__":
   main()
---
+++ code/stock_module.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+
+class StockTool :
+    def __init__(self):
+        return
+
+    def getPrice(self, id):
+        print("get called.")
+        return
